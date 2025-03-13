c0c39bc09237af80cddd252a8d8ec9b83673a958
Ernesto Juárez <ernestovanegas9@gmail.com>
2025-03-12 23:59:11 -0600

    Distribution Center Basic CRUD

--- src/handlers/distributionCenterHandler.js
+++ src/handlers/distributionCenterHandler.js
@@ -1,4 +1,4 @@
-const { createDistributionCenter } = require("../services/distributionCenterService");
+const { createDistributionCenter, getDistributionCenterById, getAllDistributionCenters } = require("../services/distributionCenterService");
 
 const create = async (event) => {
   try {
@@ -20,6 +20,48 @@ const create = async (event) => {
   }
 };
 
+const getById = async (event) => {
+  try {
+    const { id } = event.pathParameters;
+    const distributionCenter = await getDistributionCenterById(id);
+
+    if (!distributionCenter) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify({ error: "Distribution Center not found" }),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(distributionCenter),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
+
+const getAll = async (event) => {
+  try {
+    const distributionCenters = await getAllDistributionCenters();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(distributionCenters),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+}
+
 module.exports = {
   create,
+  getById,
+  getAll,
 };
